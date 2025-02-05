69ca1ccae1d9e4feea8a3c93274605a31bea1fcd
dasog <dasog@naver.com>
2025-02-05 18:41:34 +0900

    [web-netty] initalize the project

---
+++ web-netty/src/test/java/org/example/webnetty/WebNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webnetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
